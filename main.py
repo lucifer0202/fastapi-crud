5de00d535ee9285cea7954393fab20e517bcea34
Sonam Basil <sonambasil@Sonams-MacBook-Air.local>
2025-04-28 00:32:47 +0530

    Add put and delete

--- main.py
+++ main.py
@@ -22,3 +22,21 @@ def read_items(db: Session = Depends(get_db)):
 @app.post("/items/")
 def create_item(name: str, description: str, db: Session = Depends(get_db)):
     return crud.create_item(db, name, description)
+
+@app.put("/items/{item_id}")
+async def update_item(item_id: int, name: str, description: str):
+    db = SessionLocal()
+    db_item = db.query(models.Item).filter(models.Item.id == item_id).first()
+    db_item.name = name
+    db_item.description = description
+    db.commit()
+    return db_item
+
+@app.delete("/items/{item_id}")
+async def delete_item(item_id: int):
+    db = SessionLocal()
+    db_item = db.query(models.Item).filter(models.Item.id == item_id).first()
+    db.delete(db_item)
+    db.commit()
+    return {"message": "Item deleted successfully"}
+
